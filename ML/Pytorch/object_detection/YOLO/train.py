1e7d1bfbaf9f36261abc633c8035c0611f6dcf54
GiCheol.Kim <37562319+gitcheol@users.noreply.github.com>
2021-06-30 18:01:28 +0900

    Update train.py

--- ML/Pytorch/object_detection/YOLO/train.py
+++ ML/Pytorch/object_detection/YOLO/train.py
@@ -36,8 +36,8 @@ NUM_WORKERS = 2
 PIN_MEMORY = True
 LOAD_MODEL = False
 LOAD_MODEL_FILE = "overfit.pth.tar"
-IMG_DIR = "data/images"
-LABEL_DIR = "data/labels"
+IMG_DIR = "data/data/images"
+LABEL_DIR = "data/data/labels"
 
 
 class Compose(object):
